fd6f0552581e58643d01609f9cb8ad1e624fb951
Guilherme Teixeira <guist2017@hotmail.com>
2022-07-03 21:39:31 -0300

    first commit

---
+++ exercicio1.py
@@ -0,0 +1,8 @@
+largura = float(input("Digite a largura: "))
+comprimento = float(input("Digite o comprimento: "))
+
+metro = largura * comprimento
+
+w = metro * 18
+
+print("a quantidade de watts necessaria é: %.2f" % (w))
